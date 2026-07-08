f68ea304ec365f25759794b47eb9bfa60afede30
gcca <git@gcca.dev>
2026-07-08 11:11:12 -0500

    20260708111112

--- 3rdparty/fio-stl.cc
+++
@@ -1,5 +0,0 @@
-#include "fio-stl.h"
-
-extern "C" const char *monthlypy_fio_version(void) {
-  return FIO_VERSION_STRING;
-}
--- src/fio.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-extern fn monthlypy_fio_version() [*:0]const u8;
-
-pub fn ShowVersion() [:0]const u8 {
-    return std.mem.span(monthlypy_fio_version());
-}
